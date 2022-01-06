f9e467976dc07ee770c00ea4644564de037e1a0b
wp9975 <51296602+wp9975@users.noreply.github.com>
2022-01-06 18:26:21 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/librario/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.librario
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePage createState() => _HomePage();
+}
+
+class _HomePage extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Syncfusion Flutter PDF Viewer'),
+      ),
+      body: SfPdfViewer.network(
+        'https://cdn.syncfusion.com/content/PDFViewer/flutter-succinctly.pdf',
+      ),
+    );
+  }
+}
