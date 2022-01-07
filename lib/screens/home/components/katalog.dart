c29878eb854e9fd4db3366bb18316202b8cb6f9a
wp9975 <51296602+wp9975@users.noreply.github.com>
2022-01-07 21:00:47 +0100

    bottom nav

---
+++ lib/screens/home/components/katalog.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class catalog extends StatelessWidget {
+  const catalog({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: DataTable(
+        columns: [],
+        rows: [],
+      ),
+    );
+  }
+}
